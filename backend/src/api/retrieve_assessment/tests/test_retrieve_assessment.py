e2fc98e1b1aa8c37ec7ac8fe58ec7005ab34e747
Antoine Berger <antoine.berger@epitech.eu>
2025-03-05 21:11:56 +0100

    tests(retrieve_assessment): implement retrieve assessment tests

---
+++ backend/src/api/retrieve_assessment/tests/test_retrieve_assessment.py
@@ -0,0 +1,24 @@
+from unittest.mock import MagicMock
+
+from common.entities import Assessment
+from tests.__mocks__.fake_assessment_service import FakeAssessmentService
+
+from api.event import RetrieveAssessmentInput
+
+from ..app.tasks.retrieve_assessment import RetrieveAssessment
+
+
+def test_retrieve_assessment():
+    assessment = Assessment(id="AID", name="AN", role="AR", step=0, question_version="QV", findings=None)
+    assessment_service = FakeAssessmentService()
+    assessment_service.retrieve = MagicMock(return_value=assessment)
+
+    task_input = RetrieveAssessmentInput(
+        assessment_id="AID",
+    )
+    task = RetrieveAssessment(assessment_service)
+    response = task.execute(task_input)
+
+    assessment_service.retrieve.assert_called_once_with("AID")
+    assert response.status_code == 200
+    assert response.body == assessment.dict()
