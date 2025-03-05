308990e5e70abcc0d329145c44b5f54b4c73ad5d
Antoine Berger <antoine.berger@epitech.eu>
2025-03-05 21:12:05 +0100

    tests(retrieve_all_assessments): add tests for retrieving all assessments functionality

---
+++ backend/src/api/retrieve_all_assessments/tests/test_retrieve_all_assessments.py
@@ -0,0 +1,21 @@
+from unittest.mock import MagicMock
+
+from common.entities import Assessment
+from tests.__mocks__.fake_assessment_service import FakeAssessmentService
+
+from ..app.tasks.retrieve_all_assessments import RetrieveAllAssessments
+
+
+def test_retrieve_all_assessments():
+    assessments = [Assessment(id="AID", name="AN", role="AR", step=0, question_version="QV", findings=None)]
+    assessments_dict = [assessment.dict() for assessment in assessments]
+    del assessments_dict[0]["findings"]
+    assessment_service = FakeAssessmentService()
+    assessment_service.retrieve_all = MagicMock(return_value=assessments)
+
+    task = RetrieveAllAssessments(assessment_service)
+    response = task.execute(None)
+
+    assessment_service.retrieve_all.assert_called_once()
+    assert response.status_code == 200
+    assert response.body == assessments_dict
