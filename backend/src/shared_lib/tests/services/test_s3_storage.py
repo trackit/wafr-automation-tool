e645c05a85b1dae3df6fffdacf4c4d68031343dd
raxl8 <32034076+raxl8@users.noreply.github.com>
2025-03-05 15:22:04 +0100

    feat: implement unit testing

---
+++ backend/src/shared_lib/tests/services/test_s3_storage.py
@@ -0,0 +1,86 @@
+import boto3
+import pytest
+from botocore.exceptions import ClientError
+from moto import mock_aws
+from services.storage import S3Service
+from types_boto3_s3 import S3Client, S3ServiceResource
+
+
+@pytest.fixture(autouse=True)
+def s3_client():
+    with mock_aws():
+        s3_client = boto3.client("s3")
+        s3_client.create_bucket(Bucket="test-bucket")
+        s3_client.put_object(Bucket="test-bucket", Key="test-key", Body="test-body")
+        yield s3_client
+
+
+@pytest.fixture(autouse=True)
+def s3_resource():
+    with mock_aws():
+        s3_resource = boto3.resource("s3")
+        yield s3_resource
+
+
+def test_get(s3_client: S3Client, s3_resource: S3ServiceResource):
+    service = S3Service(s3_client, s3_resource)
+
+    data = service.get(Bucket="test-bucket", Key="test-key")
+    assert data == "test-body"
+
+    with pytest.raises(ClientError):
+        service.get(Bucket="test-bucket", Key="test-key-inexistent")
+
+
+def test_put(s3_client: S3Client, s3_resource: S3ServiceResource):
+    service = S3Service(s3_client, s3_resource)
+
+    service.put(Bucket="test-bucket", Key="test-key-put", Body="test-body-put")
+
+    data = s3_client.get_object(Bucket="test-bucket", Key="test-key-put")
+    assert data["Body"].read().decode("utf-8") == "test-body-put"
+
+
+def test_delete(s3_client: S3Client, s3_resource: S3ServiceResource):
+    service = S3Service(s3_client, s3_resource)
+
+    service.delete(Bucket="test-bucket", Key="test-key")
+
+    with pytest.raises(ClientError):
+        s3_client.get_object(Bucket="test-bucket", Key="test-key")
+
+
+def test_filter(s3_client: S3Client, s3_resource: S3ServiceResource):
+    service = S3Service(s3_client, s3_resource)
+
+    objects = service.filter(bucket_name="test-bucket", prefix="test-key")
+
+    assert len(objects) == 1
+    assert [obj.key for obj in objects] == ["test-key"]
+
+    s3_client.put_object(Bucket="test-bucket", Key="test-key-2", Body="test-body-2")
+    objects = service.filter(bucket_name="test-bucket", prefix="test-key")
+
+    assert len(objects) == 2
+    assert [obj.key for obj in objects] == ["test-key", "test-key-2"]
+
+
+def test_bulk_delete(s3_client: S3Client, s3_resource: S3ServiceResource):
+    service = S3Service(s3_client, s3_resource)
+
+    keys = [
+        "test-key-1",
+        "test-key-2",
+        "test-key-3",
+        "test-key-4",
+        "test-key-5",
+    ]
+
+    for key in keys:
+        s3_client.put_object(Bucket="test-bucket", Key=key, Body="test-body")
+
+    service.bulk_delete(bucket_name="test-bucket", keys=keys)
+
+    for key in keys:
+        with pytest.raises(ClientError):
+            s3_client.get_object(Bucket="test-bucket", Key=key)
