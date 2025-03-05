e506e5e2cea2e695889061c5c4e5c92faa226154
raxl8 <32034076+raxl8@users.noreply.github.com>
2025-03-05 17:06:50 +0100

    feat(api/start_assessment): throw bad request on invalid body

--- backend/src/api/start_assessment/app/main.py
+++ backend/src/api/start_assessment/app/main.py
@@ -2,6 +2,7 @@ import json
 from typing import Any
 
 import boto3
+from pydantic import ValidationError
 from tasks.start_assessment import StartAssessment
 
 from api.event import StartAssessmentInput
@@ -11,6 +12,12 @@ start_assessment_task = StartAssessment(sfn_client)
 
 
 def lambda_handler(event: dict[str, Any], _context: Any) -> dict[str, Any]:  # noqa: ANN401
-    body = json.loads(event["body"])
-    response = start_assessment_task.execute(StartAssessmentInput(**body))
-    return response.build()
+    try:
+        body = json.loads(event["body"])
+        response = start_assessment_task.execute(StartAssessmentInput(**body))
+        return response.build()
+    except ValidationError as e:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": e.errors()}),
+        }
