a44920138c6cf46d10417c724d74eeb33364196d
raxl8 <32034076+raxl8@users.noreply.github.com>
2025-03-06 10:06:39 +0100

    tests(shared_lib/utils): add questions tests

---
+++ backend/src/shared_lib/tests/utils/test_questions.py
@@ -0,0 +1,45 @@
+from unittest.mock import MagicMock, patch
+
+
+@patch("os.listdir")
+@patch("pathlib.Path.open")
+def test_retrieve_questions(mock_open: MagicMock, mock_listdir: MagicMock):
+    from utils.questions import retrieve_questions
+
+    mock_listdir.return_value = ["questions_01312025.json"]
+    mock_open.return_value.__enter__.return_value.read.return_value = """{
+        "pillar-1": {
+            "question-1": {
+                "best-practice-1": {
+                    "risk": "Low"
+                },
+                "best-practice-2": {
+                    "risk": "Medium"
+                }
+            }
+        },
+        "pillar-2": {
+            "question-2": {
+                "best-practice-1": {
+                    "risk": "High"
+                }
+            }
+        }
+    }"""
+
+    output = retrieve_questions()
+
+    assert output.question_version == "questions_01312025"
+    assert output.questions == {
+        "pillar-1": {
+            "question-1": {
+                "best-practice-1": [],
+                "best-practice-2": [],
+            }
+        },
+        "pillar-2": {
+            "question-2": {
+                "best-practice-1": [],
+            }
+        },
+    }
