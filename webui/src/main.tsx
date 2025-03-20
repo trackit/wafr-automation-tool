c83bff3849d6cd28524c25aa6f6a7e7be031b38a
alexis-ld <alexisledinh@gmail.com>
2025-03-20 18:18:46 +0100

    feat(front-end-base): Add cognito authwall

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -2,7 +2,13 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
 
+import { amplifyConfig } from './amplify.config';
+
+Amplify.configure(amplifyConfig);
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
@@ -12,7 +18,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Authenticator hideSignUp loginMechanism={'email'}>
+        <App />
+      </Authenticator>
     </QueryClientProvider>
   </StrictMode>
 );
