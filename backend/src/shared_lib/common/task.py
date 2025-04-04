4d647ba7e2c92e34be6cef0fdd39638af4eb09cf
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-04-04 15:04:50 +0200

    refactor(shared_lib): clean-up

--- backend/src/shared_lib/common/task.py
+++ backend/src/shared_lib/common/task.py
@@ -1,20 +1,7 @@
 from abc import ABC, abstractmethod
 
-from entities.ai import PromptS3Uri
-from services.storage import IStorageService
-from utils.questions import QuestionSet
-
-from state_machine.event import CreatePromptsInput
-
 
 class Task[T, U](ABC):
     @abstractmethod
     def execute(self, event: T) -> U:
         raise NotImplementedError
-
-
-class CreatePromptsTask(Task[CreatePromptsInput, list[PromptS3Uri]], ABC):
-    def __init__(self, storage_service: IStorageService, question_set: QuestionSet) -> None:
-        super().__init__()
-        self.storage_service = storage_service
-        self.question_set = question_set
