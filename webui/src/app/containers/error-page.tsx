ed96676eb2d9318bd67401e531ba78e7ac961272
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-04-25 14:15:13 +0200

    refactor: error page

--- webui/src/app/containers/error-page.tsx
+++ webui/src/app/containers/error-page.tsx
@@ -1,48 +1,25 @@
 import { paths } from '@webui/types';
-import { Modal } from '@webui/ui';
-import { useState } from 'react';
 
 export function ErrorPage(
   data: paths['/assessments/{assessmentId}']['get']['responses']['200']['content']['application/json']
 ) {
-  const [showErrorModal, setShowErrorModal] = useState<boolean>(false);
-
   return (
-    <div className="h-full">
-      <div className="flex flex-col items-center justify-center h-full gap-4">
-        <h2 className="text-center text-error font-bold">
-          An error occurred while running the assessment.
-        </h2>
-        <button
-          className="btn btn-error btn-sm text-sm h-8 min-h-8"
-          onClick={() => setShowErrorModal(true)}
-        >
-          View Error
-        </button>
-      </div>
-      <Modal
-        open={showErrorModal}
-        onClose={() => setShowErrorModal(false)}
-        className="w-full p-4"
-      >
-        <h2 className="text-2xl font-semibold text-red-600 mb-4">Error</h2>
-        <p className="text-lg text-gray-700">
-          <strong>Error Message:</strong>{' '}
-          {data?.error?.Error || 'No error message available'}
-        </p>
-        <p className="text-lg text-gray-700 mt-2">
-          <strong>Cause:</strong>{' '}
-          {data?.error?.Cause || 'No cause information available'}
-        </p>
-        <div className="mt-4 flex justify-end">
-          <button
-            onClick={() => setShowErrorModal(false)}
-            className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 transition duration-200"
-          >
-            Close
-          </button>
+    <div className="flex flex-col items-center justify-center h-full gap-4 space-y-4">
+      <h2 className="text-center text-error font-bold">
+        An error occurred while running the assessment.
+      </h2>
+      <div className="card bg-base-100 shadow-lg">
+        <div className="card-body flex items-center ">
+          <p className="text-error text-lg font-bold">
+            {data?.error?.Error || 'No error message available'}
+          </p>
+          <pre className="whitespace-pre-wrap mt-2 bg-gray-800 text-white p-2 rounded-md shadow-md">
+            <code>
+              {data?.error?.Cause || 'No cause information available'}
+            </code>
+          </pre>
         </div>
-      </Modal>
+      </div>
     </div>
   );
 }
