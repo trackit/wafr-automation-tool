e2636bc05c131fe86825644b665f0e578a17456f
Auguste mulley <mulleya3@gmail.com>
2025-05-02 09:27:48 +0400

    fix(account system): PUT update assessment

--- backend/src/api/update_assessment/app/main.py
+++ backend/src/api/update_assessment/app/main.py
@@ -1,40 +1,40 @@
 import json
+from typing import Any
 
-# ddb_resource = boto3.resource("dynamodb")
-# database_service = DDBService(ddb_resource)
-# assessment_service = AssessmentService(database_service)
-# task = UpdateAssessment(assessment_service)
+import boto3
+from pydantic import ValidationError
+from services.assessment import AssessmentService
+from services.database import DDBService
+from tasks.update_assessment import UpdateAssessment
+from utils.api import UnauthorizedError, get_bearer_token
+
+from api.event import UpdateAssessmentInput
+from entities.assessment import AssessmentDto
+
+ddb_resource = boto3.resource("dynamodb")
+database_service = DDBService(ddb_resource)
+assessment_service = AssessmentService(database_service)
+task = UpdateAssessment(assessment_service)
 
 
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Minimal Lambda OK"}),
-    }
-
-
-# def lambda_handler(event: dict[str, Any], _context: Any) -> dict[str, Any]:
-#    try:
-#        return {
-#                "statusCode": 200,
-#                "body": json.dumps({"oui": "stop here"}),
-#            }
-#        auth_header = get_bearer_token(event)
-#        body = json.loads(event["body"])
-#        assessement_dto = AssessmentDto(**body)
-#        response = task.execute(
-#            UpdateAssessmentInput(
-#                assessment_id=event["pathParameters"]["assessmentId"], assessment_dto=assessement_dto, owner_id=auth_header
-#            ),
-#        )
-#        return response.build()
-#    except UnauthorizedError as e:
-#        return {
-#            "statusCode": 401,
-#            "body": json.dumps({"error": str(e)}),
-#        }
-#    except ValidationError as e:
-#        return {
-#            "statusCode": 400,
-#            "body": json.dumps({"error": e.errors()}),
-#        }
+def lambda_handler(event: dict[str, Any], _context: Any) -> dict[str, Any]:
+   try:
+       auth_header = get_bearer_token(event)
+       body = json.loads(event["body"])
+       assessement_dto = AssessmentDto(**body)
+       response = task.execute(
+           UpdateAssessmentInput(
+               assessment_id=event["pathParameters"]["assessmentId"], assessment_dto=assessement_dto, owner_id=auth_header
+           ),
+       )
+       return response.build()
+   except UnauthorizedError as e:
+       return {
+           "statusCode": 401,
+           "body": json.dumps({"error": str(e)}),
+       }
+   except ValidationError as e:
+       return {
+           "statusCode": 400,
+           "body": json.dumps({"error": e.errors()}),
+       }
