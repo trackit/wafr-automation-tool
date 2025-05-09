8b0fc12ab4b7ea6beb5eb3643f79a73ab0cf5e76
Lucas Sanchez <lucas.sanchez.dev@gmail.com>
2025-05-09 16:14:28 +0200

    feat(WAFR-64): add di package

---
+++ libs/shared/di-container/index.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+
+export { DIToken, createInjectionToken } from './DIToken';
+export { inject } from './inject';
+export { reset } from './reset';
+export {
+  register,
+} from './register';
+export { FactoryFunction } from './FactoryFunction';
+export {
+  FactoryProvider, ValueProvider, ClassProvider, Provider,
+} from './Provider';
