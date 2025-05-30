036a3abbadfbfd92b399fc46aa77ff405043e884
Lucas Sanchez <lucas.sanchez.dev@gmail.com>
2025-05-30 10:27:55 +0200

    feat(WAFR-98): implement DeleteAssessment useCase

--- libs/backend/useCases/src/lib/deleteAssessment/deleteAssessment.ts
+++ libs/backend/useCases/src/lib/deleteAssessment/deleteAssessment.ts
@@ -12,21 +12,50 @@ export type DeleteAssessmentUseCaseArgs = {
 };
 
 export interface DeleteAssessmentUseCase {
-  deleteAssessment(
-    args: DeleteAssessmentUseCaseArgs
-  ): Promise<{ assessmentId: string }>;
+  deleteAssessment(args: DeleteAssessmentUseCaseArgs): Promise<void>;
 }
 
 export class DeleteAssessmentUseCaseImpl implements DeleteAssessmentUseCase {
-  private readonly stateMachine = inject(tokenAssessmentsStateMachine);
-  private readonly repository = inject(tokenAssessmentsRepository);
+  private readonly assessmentsStateMachine = inject(
+    tokenAssessmentsStateMachine
+  );
+  private readonly assessmentsRepository = inject(tokenAssessmentsRepository);
+
+  private async deleteAssessmentFromRepository(args: {
+    assessmentId: string;
+    organization: string;
+  }): Promise<void> {
+    const { assessmentId, organization } = args;
+    await this.assessmentsRepository.deleteFindings({
+      assessmentId,
+      organization,
+    });
+    await this.assessmentsRepository.delete({
+      assessmentId,
+      organization,
+    });
+  }
 
   public async deleteAssessment(
     args: DeleteAssessmentUseCaseArgs
-  ): Promise<{ assessmentId: string }> {
-    throw new Error(
-      'DeleteAssessmentUseCaseImpl.deleteAssessment is not implemented yet'
-    );
+  ): Promise<void> {
+    const assessment = await this.assessmentsRepository.getOne({
+      assessmentId: args.assessmentId,
+      organization: args.user.organizationDomain,
+    });
+    if (!assessment) {
+      throw new NotFoundError(
+        `Assessment with id ${args.assessmentId} not found for organization ${args.user.organizationDomain}`
+      );
+    }
+
+    await Promise.all([
+      this.deleteAssessmentFromRepository({
+        assessmentId: args.assessmentId,
+        organization: args.user.organizationDomain,
+      }),
+      this.assessmentsStateMachine.cancelAssessment(assessment.executionArn),
+    ]);
   }
 }
 
