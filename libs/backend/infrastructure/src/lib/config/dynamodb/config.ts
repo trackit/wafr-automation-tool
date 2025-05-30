590463a9e18151f2d196f8972ce96cce95e344ac
Lucas Sanchez <lucas.sanchez.dev@gmail.com>
2025-05-30 10:19:13 +0200

    feat(WAFR-98): move dynamo db config

---
+++ libs/backend/infrastructure/src/lib/config/dynamodb/config.ts
@@ -0,0 +1,62 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClientConfig } from '@aws-sdk/client-dynamodb/dist-types/DynamoDBClient';
+
+import { createInjectionToken, inject } from '@shared/di-container';
+import { assertIsDefined } from '@shared/utils';
+
+export type DynamoDBConfig = {
+  region: string;
+  endpoint?: string;
+};
+
+export const defaultDynamoDbConfig: DynamoDBClientConfig = {
+  region: process.env.AWS_REGION,
+} as const;
+
+export const testDynamoDbConfig: DynamoDBClientConfig = {
+  endpoint: 'http://127.0.0.1:8000',
+  region: 'us-west-2',
+};
+
+export const tokenDynamoDBConfig = createInjectionToken<DynamoDBClientConfig>(
+  'DynamoDBConfig',
+  {
+    useValue: defaultDynamoDbConfig,
+  }
+);
+
+export const tokenDynamoDBClient = createInjectionToken<DynamoDBClient>(
+  'DynamoDBClient',
+  {
+    useFactory: () => new DynamoDBClient(inject(tokenDynamoDBConfig)),
+  }
+);
+
+export const tokenDynamoDBDocument = createInjectionToken<DynamoDBDocument>(
+  'DynamoDBDocument',
+  {
+    useFactory: () =>
+      DynamoDBDocument.from(inject(tokenDynamoDBClient), {
+        marshallOptions: {
+          removeUndefinedValues: true,
+        },
+      }),
+  }
+);
+
+export const tokenDynamoDBTableName = createInjectionToken<string>(
+  'tokenDynamoDBTableName',
+  {
+    useFactory: () => {
+      const tableName = process.env.DDB_TABLE;
+      assertIsDefined(tableName, 'DDB_TABLE is not defined');
+      return tableName;
+    },
+  }
+);
+
+export const tokenDynamoDBBatchSize = createInjectionToken<number>(
+  'tokenDynamoDBBatchSize',
+  { useValue: 25 } // Default batch size for DynamoDB operations
+);
