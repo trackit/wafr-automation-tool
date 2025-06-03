ac7c6685cccf6348844418870778506f60d51425
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-06-03 20:29:02 +0200

    feat(WAFR-101): implement getAll in AssessmentsRepositoryDynamoDB with tests

--- libs/backend/ports/src/lib/AssessmentsRepository.ts
+++ libs/backend/ports/src/lib/AssessmentsRepository.ts
@@ -8,6 +8,15 @@ export interface AssessmentsRepository {
     scanningTool: string;
     finding: Finding;
   }): Promise<void>;
+  getAll(args: {
+    organization: string;
+    limit?: number;
+    search?: string;
+    nextToken?: string;
+  }): Promise<{
+    assessments: Assessment[];
+    nextToken?: string;
+  }>;
   get(args: {
     assessmentId: string;
     organization: string;
