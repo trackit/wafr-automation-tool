642879c9a2897ead4c9b0fc18aa72a28e6672633
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-06-12 15:36:18 +0200

    feat(WAFR-117): implement updatePillar handler with tests

---
+++ apps/backend/src/handlers/updatePillar/UpdatePillarAdapter.test.ts
@@ -0,0 +1,137 @@
+import {
+  NoContentError,
+  NotFoundError,
+  tokenUpdatePillarUseCase,
+} from '@backend/useCases';
+import { register, reset } from '@shared/di-container';
+
+import { APIGatewayProxyEventMother } from '../../utils/APIGatewayProxyEventMother';
+import { UpdatePillarAdapter } from './UpdatePillarAdapter';
+import { UpdatePillarAdapterEventMother } from './UpdatePillarAdapterEventMother';
+
+describe('UpdatePillarAdapter', () => {
+  describe('args validation', () => {
+    it('should validate args parameters', async () => {
+      const { adapter } = setup();
+
+      const event = UpdatePillarAdapterEventMother.basic()
+        .withAssessmentId('assessment-id')
+        .withPillarId('1')
+        .withBody({ disabled: true })
+        .build();
+      const response = await adapter.handle(event);
+
+      expect(response.statusCode).not.toBe(400);
+    });
+
+    it('should return a 400 without path parameters', async () => {
+      const { adapter } = setup();
+
+      const event = APIGatewayProxyEventMother.basic()
+        .withPathParameters(null)
+        .build();
+
+      const response = await adapter.handle(event);
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('should return a 400 with invalid body parameters', async () => {
+      const { adapter } = setup();
+
+      const event = APIGatewayProxyEventMother.basic()
+        .withPathParameters({
+          assessmentId: 'assessment-id',
+          pillarId: '1',
+        })
+        .withBody(JSON.stringify({ test: true }))
+        .build();
+
+      const response = await adapter.handle(event);
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('should return a 400 with undefined body parameters', async () => {
+      const { adapter } = setup();
+
+      const event = APIGatewayProxyEventMother.basic()
+        .withPathParameters({
+          assessmentId: 'assessment-id',
+          pillarId: '1',
+        })
+        .build();
+
+      const response = await adapter.handle(event);
+      expect(response.statusCode).toBe(400);
+    });
+  });
+
+  describe('useCase call and status code', () => {
+    it('should call useCase with path parameters and PillarBody', async () => {
+      const { adapter, useCase } = setup();
+
+      const event = UpdatePillarAdapterEventMother.basic()
+        .withAssessmentId('assessment-id')
+        .withPillarId('1')
+        .withBody({ disabled: true })
+        .build();
+
+      await adapter.handle(event);
+
+      expect(useCase.updatePillar).toHaveBeenCalledExactlyOnceWith(
+        expect.objectContaining({
+          assessmentId: 'assessment-id',
+          pillarId: '1',
+          pillarBody: { disabled: true },
+        })
+      );
+    });
+
+    it('should return a 200 status code', async () => {
+      const { adapter } = setup();
+
+      const event = UpdatePillarAdapterEventMother.basic()
+        .withAssessmentId('assessment-id')
+        .withPillarId('1')
+        .withBody({ disabled: true })
+        .build();
+
+      const response = await adapter.handle(event);
+      expect(response.statusCode).toBe(200);
+    });
+
+    it('should return a 204 if useCase throws a NoContentError', async () => {
+      const { adapter, useCase } = setup();
+
+      const event = UpdatePillarAdapterEventMother.basic()
+        .withAssessmentId('assessment-id')
+        .withPillarId('1')
+        .withBody({})
+        .build();
+      useCase.updatePillar.mockRejectedValue(new NoContentError());
+
+      const response = await adapter.handle(event);
+      expect(response.statusCode).toBe(204);
+    });
+
+    it('should return a 404 if useCase throws a NotFoundError', async () => {
+      const { adapter, useCase } = setup();
+
+      const event = UpdatePillarAdapterEventMother.basic()
+        .withAssessmentId('assessment-id')
+        .withPillarId('1')
+        .withBody({ disabled: true })
+        .build();
+      useCase.updatePillar.mockRejectedValue(new NotFoundError());
+
+      const response = await adapter.handle(event);
+      expect(response.statusCode).toBe(404);
+    });
+  });
+});
+
+const setup = () => {
+  reset();
+  const useCase = { updatePillar: vitest.fn() };
+  register(tokenUpdatePillarUseCase, { useValue: useCase });
+  return { useCase, adapter: new UpdatePillarAdapter() };
+};
---
+++ apps/backend/src/handlers/updatePillar/UpdatePillarAdapter.ts
@@ -0,0 +1,73 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { z, ZodError, ZodType } from 'zod';
+
+import type { operations } from '@shared/api-schema';
+import { inject } from '@shared/di-container';
+
+import { tokenUpdatePillarUseCase } from '@backend/useCases';
+import { JSONParseError, parseJson } from '@shared/utils';
+import { BadRequestError } from '../../utils/HttpError';
+import { getUserFromEvent } from '../../utils/getUserFromEvent/getUserFromEvent';
+import { handleHttpRequest } from '../../utils/handleHttpRequest';
+
+const UpdatePillarPathSchema = z.object({
+  assessmentId: z.string(),
+  pillarId: z.string(),
+}) satisfies ZodType<operations['updatePillar']['parameters']['path']>;
+
+const UpdatePillarBodySchema = z
+  .object({
+    disabled: z.boolean().optional(),
+  })
+  .strict() satisfies ZodType<
+  operations['updatePillar']['requestBody']['content']['application/json']
+>;
+
+export class UpdatePillarAdapter {
+  private readonly useCase = inject(tokenUpdatePillarUseCase);
+
+  private parseBody(
+    body?: string
+  ): operations['updatePillar']['requestBody']['content']['application/json'] {
+    const parsedBody = parseJson(body);
+    return UpdatePillarBodySchema.parse(parsedBody);
+  }
+
+  public async handle(
+    event: APIGatewayProxyEvent
+  ): Promise<APIGatewayProxyResult> {
+    return handleHttpRequest({
+      event,
+      func: this.processRequest.bind(this),
+      statusCode: 200,
+    });
+  }
+
+  private async processRequest(event: APIGatewayProxyEvent): Promise<void> {
+    const { pathParameters, body } = event;
+    if (!pathParameters) {
+      throw new BadRequestError('Missing path parameters');
+    }
+    if (!body) {
+      throw new BadRequestError('Missing body parameters');
+    }
+
+    try {
+      const parsedPath = UpdatePillarPathSchema.parse(pathParameters);
+      const parsedBody = this.parseBody(body);
+      await this.useCase.updatePillar({
+        user: getUserFromEvent(event),
+        assessmentId: parsedPath.assessmentId,
+        pillarId: parsedPath.pillarId,
+        pillarBody: parsedBody,
+      });
+    } catch (e) {
+      if (e instanceof ZodError) {
+        throw new BadRequestError(`Invalid parameters: ${e.message}`);
+      } else if (e instanceof JSONParseError) {
+        throw new BadRequestError(`Invalid JSON in request body: ${e.message}`);
+      }
+      throw e;
+    }
+  }
+}
