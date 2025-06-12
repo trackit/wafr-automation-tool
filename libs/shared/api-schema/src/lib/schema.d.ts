4ebcdfede7ef88c0cda637dbe4577f50e88da8e6
Lucas Sanchez <lucas.sanchez.dev@gmail.com>
2025-06-12 11:06:07 +0200

    docs(WAFR-115): add openapi 204 return for updateBestPractice endpoint

--- libs/shared/api-schema/src/lib/schema.d.ts
+++ libs/shared/api-schema/src/lib/schema.d.ts
@@ -846,6 +846,13 @@ export interface operations {
                 };
                 content?: never;
             };
+            /** @description The best practice has no fields to update */
+            204: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
             /** @description Invalid request body or a issue occurred while trying to retrieve the organization of the user */
             400: {
                 headers: {
