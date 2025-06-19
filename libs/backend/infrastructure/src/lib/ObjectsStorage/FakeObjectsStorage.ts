0f0e02bf67b5e0754dace3ad5eb565bd6faf20d1
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-06-19 16:54:33 +0200

    refactor(WAFR-124): enhance FakeObjectsStorage with put and improved list method for object management

--- libs/backend/infrastructure/src/lib/ObjectsStorage/FakeObjectsStorage.ts
+++ libs/backend/infrastructure/src/lib/ObjectsStorage/FakeObjectsStorage.ts
@@ -1,15 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { ObjectsStorage } from '@backend/ports';
 import { createInjectionToken } from '@shared/di-container';
 
 export class FakeObjectsStorage implements ObjectsStorage {
+  public objects: Record<string, string> = {};
+
+  put(args: { key: string; object: string }): Promise<void> {
+    this.objects[args.key] = args.object;
+    return Promise.resolve();
+  }
+
   list(args: { prefix: string }): Promise<string[]> {
-    // No-op for fake implementation
-    return Promise.resolve([]);
+    const objects = Object.values(this.objects).filter((object) =>
+      object.startsWith(args.prefix)
+    );
+    return Promise.resolve(objects);
   }
 
   bulkDelete(args: { keys: string[] }): Promise<void> {
-    // No-op for fake implementation
+    args.keys.forEach((key) => delete this.objects[key]);
     return Promise.resolve();
   }
 }
