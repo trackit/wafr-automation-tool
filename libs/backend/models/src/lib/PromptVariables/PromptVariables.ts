d0d5dc9bfa943ea863f1e6866fd8f25ad4d726fb
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-06-25 19:28:50 +0200

    refactor(WAFR-128): enhance PromptVariables model to use AIBestPracticeMetadata and Finding types

--- libs/backend/models/src/lib/PromptVariables/PromptVariables.ts
+++ libs/backend/models/src/lib/PromptVariables/PromptVariables.ts
@@ -1,5 +1,7 @@
+import { AIBestPracticeMetadata, AIFinding } from '../AI';
+
 export interface PromptVariables {
   scanningToolTitle: string;
-  questionSetData: string;
-  scanningToolData: string;
+  questionSetData: AIBestPracticeMetadata[];
+  scanningToolData: AIFinding[];
 }
--- libs/backend/models/src/lib/PromptVariables/PromptVariablesMother.ts
+++ libs/backend/models/src/lib/PromptVariables/PromptVariablesMother.ts
@@ -1,3 +1,5 @@
+import { AIBestPracticeMetadata } from '../AI';
+import { Finding } from '../Finding';
 import type { PromptVariables } from './PromptVariables';
 
 export class PromptVariablesMother {
@@ -10,8 +12,8 @@ export class PromptVariablesMother {
   public static basic(): PromptVariablesMother {
     return new PromptVariablesMother({
       scanningToolTitle: 'Scanning Tool Title',
-      questionSetData: 'Question Set Data',
-      scanningToolData: 'Scanning Tool Data',
+      questionSetData: [],
+      scanningToolData: [],
     });
   }
 
@@ -22,12 +24,16 @@ export class PromptVariablesMother {
     return this;
   }
 
-  public withQuestionSetData(questionSetData: string): PromptVariablesMother {
+  public withQuestionSetData(
+    questionSetData: AIBestPracticeMetadata[]
+  ): PromptVariablesMother {
     this.data.questionSetData = questionSetData;
     return this;
   }
 
-  public withScanningToolData(scanningToolData: string): PromptVariablesMother {
+  public withScanningToolData(
+    scanningToolData: Finding[]
+  ): PromptVariablesMother {
     this.data.scanningToolData = scanningToolData;
     return this;
   }
