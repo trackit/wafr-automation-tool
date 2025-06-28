540798d32416e63fbf9fb3a5375d818ad85c74b4
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-06-28 22:45:26 +0200

    feat(WAFR-128): update AIBestPracticePort, AssessmentRepository, ObjectsStorage and add AIService

--- libs/backend/ports/src/lib/ObjectsStorage.ts
+++ libs/backend/ports/src/lib/ObjectsStorage.ts
@@ -1,5 +1,7 @@
 export interface ObjectsStorage {
+  get(args: { key: string }): Promise<string>;
   list(args: { prefix: string }): Promise<string[]>;
   bulkDelete(args: { keys: string[] }): Promise<void>;
   put(args: { key: string; body: string }): Promise<void>;
+  parseURI(uri: string): { bucket: string; key: string };
 }
