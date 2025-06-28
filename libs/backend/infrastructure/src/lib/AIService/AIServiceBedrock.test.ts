1b4585e3dc5bd22614fc439d71005b1275edfc3c
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-06-28 22:02:05 +0200

    feat(WAFR-128): implement AIService with Bedrock integration and mock tests for stream handling

---
+++ libs/backend/infrastructure/src/lib/AIService/AIServiceBedrock.test.ts
@@ -0,0 +1,140 @@
+import { mockClient } from 'aws-sdk-client-mock';
+
+import { inject, reset } from '@shared/di-container';
+
+import {
+  ConverseStreamCommand,
+  ConverseStreamOutput,
+} from '@aws-sdk/client-bedrock-runtime';
+import {
+  AIBestPracticeMetadataMother,
+  AIFindingMother,
+  PromptVariablesMother,
+} from '@backend/models';
+import { registerTestInfrastructure } from '../registerTestInfrastructure';
+import {
+  AIServiceBedrock,
+  tokenClientBedrockRuntime,
+} from './AIServiceBedrock';
+
+const fakeChunks: Partial<ConverseStreamOutput>[] = [
+  {
+    contentBlockDelta: {
+      delta: { text: 'Hello' },
+      contentBlockIndex: undefined,
+    },
+  },
+  {
+    contentBlockDelta: {
+      delta: { text: ' world' },
+      contentBlockIndex: undefined,
+    },
+  },
+];
+
+describe('AIService Infrastructure', () => {
+  describe('converse', () => {
+    it('should call bedrock converse stream with proper parameters', async () => {
+      const { aiServiceBedrock, bedrockRuntimeClientMock } = setup();
+
+      bedrockRuntimeClientMock.on(ConverseStreamCommand).resolves({
+        stream: (async function* () {
+          for (const chunk of fakeChunks) {
+            yield chunk as ConverseStreamOutput;
+          }
+        })(),
+        $metadata: { httpStatusCode: 200 },
+      });
+      const promptVariables = PromptVariablesMother.basic()
+        .withQuestionSetData([AIBestPracticeMetadataMother.basic().build()])
+        .withScanningToolData([AIFindingMother.basic().build()])
+        .withScanningToolTitle('Test Scanning Tool Title')
+        .build();
+
+      await expect(
+        aiServiceBedrock.converse({
+          promptArn:
+            'arn:aws:bedrock:us-east-1:123456789012:prompt/test-prompt',
+          promptVariables: promptVariables,
+        })
+      ).resolves.toEqual('Hello world');
+
+      const converseStreamCalls = bedrockRuntimeClientMock.commandCalls(
+        ConverseStreamCommand
+      );
+      expect(converseStreamCalls).toHaveLength(1);
+      const converseExecutionCall = converseStreamCalls[0];
+      expect(converseExecutionCall.args[0].input).toEqual({
+        modelId: 'arn:aws:bedrock:us-east-1:123456789012:prompt/test-prompt',
+        promptVariables: Object.entries(promptVariables).reduce(
+          (promptVariables, [key, value]) => ({
+            ...promptVariables,
+            [key]: { text: JSON.stringify(value) },
+          }),
+          {}
+        ),
+      });
+    });
+
+    it('should throw an exception if the converse stream fails', async () => {
+      const { aiServiceBedrock, bedrockRuntimeClientMock } = setup();
+
+      bedrockRuntimeClientMock.on(ConverseStreamCommand).resolves({
+        stream: (async function* () {
+          for (const chunk of fakeChunks) {
+            yield chunk as ConverseStreamOutput;
+          }
+        })(),
+        $metadata: { httpStatusCode: 500 },
+      });
+      const promptVariables = PromptVariablesMother.basic()
+        .withQuestionSetData([AIBestPracticeMetadataMother.basic().build()])
+        .withScanningToolData([AIFindingMother.basic().build()])
+        .withScanningToolTitle('Test Scanning Tool Title')
+        .build();
+
+      await expect(
+        aiServiceBedrock.converse({
+          promptArn:
+            'arn:aws:bedrock:us-east-1:123456789012:prompt/test-prompt',
+          promptVariables: promptVariables,
+        })
+      ).rejects.toThrow(Error);
+    });
+
+    it('should throw an exception if the converse stream return an undefined stream', async () => {
+      const { aiServiceBedrock, bedrockRuntimeClientMock } = setup();
+
+      bedrockRuntimeClientMock.on(ConverseStreamCommand).resolves({
+        stream: undefined,
+        $metadata: { httpStatusCode: 200 },
+      });
+      const promptVariables = PromptVariablesMother.basic()
+        .withQuestionSetData([AIBestPracticeMetadataMother.basic().build()])
+        .withScanningToolData([AIFindingMother.basic().build()])
+        .withScanningToolTitle('Test Scanning Tool Title')
+        .build();
+
+      await expect(
+        aiServiceBedrock.converse({
+          promptArn:
+            'arn:aws:bedrock:us-east-1:123456789012:prompt/test-prompt',
+          promptVariables: promptVariables,
+        })
+      ).rejects.toThrow(Error);
+    });
+  });
+});
+
+const setup = () => {
+  reset();
+  registerTestInfrastructure();
+  const aiServiceBedrock = new AIServiceBedrock();
+  const bedrockRuntimeClientMock = mockClient(
+    inject(tokenClientBedrockRuntime)
+  );
+  return {
+    aiServiceBedrock,
+    bedrockRuntimeClientMock,
+  };
+};
