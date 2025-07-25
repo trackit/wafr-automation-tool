2c76a690c8ba6700d5ad505f494199cbe4bb44c4
raxl8 <32034076+raxl8@users.noreply.github.com>
2025-07-25 11:23:51 +0200

    chore(WAFR-148): remove unused console.log

--- apps/webui/libs/api-client/src/lib/exportToAWS.ts
+++ apps/webui/libs/api-client/src/lib/exportToAWS.ts
@@ -7,7 +7,6 @@ export function exportToAWS(
   }: paths['/assessments/{assessmentId}/exports/well-architected-tool']['post']['parameters']['path'],
   requestBody: paths['/assessments/{assessmentId}/exports/well-architected-tool']['post']['requestBody']['content']['application/json']
 ) {
-  console.log('exportToAWS', requestBody);
   return apiClient.post(
     `/assessments/${assessmentId}/exports/well-architected-tool`,
     requestBody
