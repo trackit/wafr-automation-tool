45b7edc448704056ba17ceacbb6955d55facc059
raxl8 <32034076+raxl8@users.noreply.github.com>
2025-08-12 12:46:59 +0200

    fix(WAFR-164): don't delete cloudformation templates from cloudfront bucket

--- scripts/frontend/deploy.sh
+++ scripts/frontend/deploy.sh
@@ -4,5 +4,5 @@ stack_outputs=$(sam list stack-outputs --stack-name="wafr-automation-tool-$STAGE
 bucket=$(echo "$stack_outputs" | jq -r -c '.[] | select(.OutputKey=="CloudFrontS3BucketName") | .OutputValue')
 distribution_id=$(echo "$stack_outputs" | jq -r -c '.[] | select(.OutputKey=="CloudFrontDistributionId") | .OutputValue')
 
-aws s3 sync ./dist/webui "s3://${bucket}/" --delete
+aws s3 sync ./dist/webui "s3://${bucket}/" --delete --exclude "cloudformation-templates/*"
 aws cloudfront create-invalidation --distribution-id "${distribution_id}" --paths "/*" --no-cli-pager
