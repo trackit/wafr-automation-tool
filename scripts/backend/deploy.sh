e0ed393878cb37575ebcfa0846b3abda53fa2626
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-10-09 17:20:47 +0200

    feat: add deploy environment variables

--- scripts/backend/deploy.sh
+++ scripts/backend/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PARAMS="Owner=\"$STAGE\" Debug=\"$DEBUG\" InitialUserEmail=\"$INITIAL_USER_EMAIL\""
+PARAMS="Owner=\"$STAGE\" Debug=\"$DEBUG\" InitialUserEmail=\"$INITIAL_USER_EMAIL\" LogRetentionInDays=\"$LOG_RETENTION_IN_DAYS\" GenAIMaxRetries=\"$GEN_AI_MAX_RETRIES\" AIFindingsAssociationChunkSize=\"$AI_FINDINGS_ASSOCIATION_CHUNK_SIZE\""
 
 if [ -n "$UNIT_BASED_PRICING_PRODUCT_CODE" ]; then
   PARAMS="$PARAMS UnitBasedPricingProductCode=\"$UNIT_BASED_PRICING_PRODUCT_CODE\""
