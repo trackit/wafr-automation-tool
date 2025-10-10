423c963c26fcb4231faa304910be9ea835ed030e
antoine-berger <64917674+antoine-berger@users.noreply.github.com>
2025-10-10 16:18:19 +0200

    fix: remove findingRepository models & mapping export

--- libs/backend/infrastructure/src/lib/FindingsRepository/index.ts
+++ libs/backend/infrastructure/src/lib/FindingsRepository/index.ts
@@ -1,5 +1,3 @@
 export * from './FakeFindingsRepository';
 export * from './FindingsRepository.token';
 export * from './FindingsRepositoryDynamoDB';
-export * from './FindingsRepositoryDynamoDBMapping';
-export * from './FindingsRepositoryDynamoDBModels';
