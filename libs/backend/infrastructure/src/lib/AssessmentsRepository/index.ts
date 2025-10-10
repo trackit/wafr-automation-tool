ce79687072cb7b2e7cefba45ec6f0b5d161dbee9
Lucas Sanchez <lucas.sanchez.dev@gmail.com>
2025-10-10 11:04:36 +0200

    feat: export dynamodb models

--- libs/backend/infrastructure/src/lib/AssessmentsRepository/index.ts
+++ libs/backend/infrastructure/src/lib/AssessmentsRepository/index.ts
@@ -1,4 +1,6 @@
 export * from './AssessmentsRepository.token';
 export * from './AssessmentsRepositoryDynamoDB';
+export * from './AssessmentsRepositoryDynamoDBMapping';
+export * from './AssessmentsRepositoryDynamoDBModels';
 export * from './AssessmentsRepositorySQL';
 export * from './FakeAssessmentsRepository';
